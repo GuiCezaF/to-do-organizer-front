b7ddddb2f67f017bf8175163ffe9b17f64408b12
Guilherme Felipe <contato.guicezafe@gmail.com>
2024-06-07 16:50:50 -0300

    feat: adicionado o tailwind, configurando system tray e limpando o App.tsx

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,17 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::{CustomMenuItem, SystemTrayMenu, SystemTray};
+
 
 fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let tray_menu = SystemTrayMenu::new().add_item(quit);
+    let tray = SystemTray::new().with_menu(tray_menu);
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    .system_tray(tray)
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+    
 }
